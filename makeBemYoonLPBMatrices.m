cdbaf5b58801db3c3d2d380e79ff587d4ef371b7
Jay Bardhan <jbardhan@alum.mit.edu>
2014-09-22 22:37:30 -0400

    Fixing YoonLPB to look and act like YoonDiel structure

--- makeBemYoonLPBMatrices.m
+++ makeBemYoonLPBMatrices.m
@@ -11,9 +11,14 @@ Idiel = eye(npDiel);
 
 dielDielLaplace = makeSurfaceToSurfaceLaplaceOperators(dielSrf);
 dielDielYukawa =  makeSurfaceToSurfaceYukawaOperators(dielSrf);
-			     
-A = [(Idiel/2 + dielDielLaplace.K) -dielDielLaplace.V ;
-     (Idiel/2 - dielDielYukawa.K) ((epsIn/epsOut)*dielDielYukawa.V)];
+
+A11 = (Idiel/2 + dielDielLaplace.K);
+A12 = -dielDielLaplace.V ;
+A21 = (Idiel/2 - dielDielYukawa.K);
+A22_base = dielDielYukawa.V;
+
+A = [A11 A12;
+     A21 (epsIn/epsOut)*A22_base];
 
 B = [dielChargeOp.phiCoul/epsIn;
      zeros(npDiel,nCharges);];
@@ -22,4 +27,7 @@ C = 4*pi*[-dielChargeOp.dlpToCharges dielChargeOp.slpToCharges];
 
 bem = struct('B', B, 'A', A, 'C', C,...
 	     'dielChargeOp',dielChargeOp,...
-	     'dielDielOp',dielDielOp);
+	     'dielDielLaplace',dielDielLaplace,...
+	     'dielDielYukawa',dielDielYukawa,...
+	     'A11',A11,'A12',A12,...
+	     'A21',A21,'A22_base',A22_base);
